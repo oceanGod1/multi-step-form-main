34786197d0c6795ca41564092547e824bdacf77c
kingsley <kingsleyjackfestus@gmail.com>
2024-12-18 16:21:06 +0100

    Testing if the gh server works
    The content of App.jsx have been replaced with Hello

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import MotherCmp from "./MotherCmp/MotherCmp";
+
+function App() {
+  return (
+    <>
+      {/* <MotherCmp /> */}
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default App;
